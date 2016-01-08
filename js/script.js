2e0fa834daadce0e20b345e7897b60c20e8189fa
martmwangi <luthermwangi2@gmail.com>
2016-01-08 11:59:37 +0300

    Scrollit didnot work needs sections

--- js/script.js
+++ js/script.js
@@ -1,34 +1,14 @@
-      google.load("visualization", "1.1", {packages:["bar"]});
-      google.setOnLoadCallback(drawStuff);
-
-      function drawStuff() {
-        var data = new google.visualization.arrayToDataTable([
-          ['Languages', 'Percentage'],
-          ["HTML", 65],
-          ["CSS", 57],
-          ["Javascript", 38],
-          ["Python", 35],
-          ['Java', 25]
-        ]);
-
-        var options = {
-          title: '',
-          width: 900,
-          legend: { position: 'none' },
-          chart: { title: '',
-                   subtitle: '' },
-          bars: 'horizontal', // Required for Material Bar Charts.
-          axes: {
-            x: {
-              0: { side: 'top', label: 'Percentage'} // Top x-axis.
-            }
-          },
-          bar: { groupWidth: "90%" }
-        };
-
-        var chart = new google.charts.Bar(document.getElementById('top_x_div'));
-        chart.draw(data, options);
-      };
+//Scrollify configuration
+$.scrollify({
+        section : ".section-class-name",
+        sectionName : "section-name",
+        easing: "easeOutExpo",
+        scrollSpeed: 1100,
+        offset : 0,
+        scrollbars: true,
+        before:function() {},
+        after:function() {},
+        afterResize:function() {}
 //Twitter login
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 //For horizontal scroll
